a96776845cceb15c339d9bfc8ad45649d1568e53
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-10-08 17:40:03 -0500

    add simple api rest for learn

---
+++ csharp/firstApi/Db.cs
@@ -0,0 +1,67 @@
+namespace FirstApi.DB;
+
+public record Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(int id, string name, int age)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+    }
+}
+
+public class PeopleDb
+{
+    private List<Person> _people = new List<Person>();
+    public PeopleDb(uint total)
+    {
+
+        for (int i = 0; i < 20; i++)
+        {
+            _people.Add(
+                new Person(
+                    i + 1,
+                    $"Person {i + 1}",
+                    new Random().Next(18, 60) // Edad aleatoria entre 18 y 60
+                )
+            );
+        }
+    }
+    public List<Person> GetAll() => _people;
+    public Person? GetOne(int id) => _people.SingleOrDefault(p => p.Id == id);
+    public Person CreatePerson(Person p)
+    {
+        _people.Add(p);
+        return p;
+    }
+    public Person? UpdateName(int id, string newName)
+    {
+        Person? finded = GetOne(id);
+        if (finded == null)
+        {
+            return null;
+        }
+        _people = _people.Select(p =>
+        {
+            if (p.Id == id) p.Name = newName;
+            return p;
+        }).ToList();
+
+        return new Person(id, newName, finded.Age);
+    }
+    // public void 
+    public Person? Delete(int id)
+    {
+        Person? finded = GetOne(id);
+        if (finded == null)
+        {
+            return null;
+        }
+        _people = _people.FindAll(p => p.Id != id).ToList();
+        return finded;
+    }
+}
+
---
+++ csharp/firstApi/Program.cs
@@ -0,0 +1,77 @@
+using Microsoft.OpenApi.Models;
+
+using FirstApi.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+// Setup the wagger documentation.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FIRST API", Description = "First app for web API", Version = "v1" });
+});
+var app = builder.Build();
+
+
+PeopleDb Store = new PeopleDb(20);
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/persons", () =>
+{
+    return new Response<List<Person>>
+    {
+        Message = "People obtained.",
+        Data = Store.GetAll()
+    };
+});
+app.MapGet("/persons/{id}", (int id) =>
+{
+    Person? p = Store.GetOne(id);
+    return new Response<Person>
+    {
+        Message = p == null ? "Person not found." : "Person obtained.",
+        Data = p
+    };
+});
+app.MapPost("/persons", (Person p) =>
+{
+    return new Response<Person>
+    {
+        Message = "Person created!.",
+        Data = Store.CreatePerson(p)
+    };
+
+});
+app.MapPut("/persons/{id}", (int id, string name) =>
+{
+    Person? updated = Store.UpdateName(id, name);
+    return new Response<Person>
+    {
+        Message = updated == null ? "Person not found" : "Person updated!",
+        Data = updated,
+    };
+});
+
+app.MapDelete("/persons/{id}", (int id) =>
+{
+    Person? p = Store.Delete(id);
+    return new Response<Person>
+    {
+        Message = p == null ? "Person not found." : "Person removed!",
+        Data = p,
+    };
+});
+// Read the swagger configuration
+// And execute the middleware
+app.UseSwagger();
+app.UseSwaggerUI((c) =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "First API in C#");
+});
+
+app.Run();
+
+struct Response<T>
+{
+    public string Message { get; set; }
+    public T? Data { get; set; }
+}
