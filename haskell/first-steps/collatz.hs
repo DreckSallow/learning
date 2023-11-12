ca127dbd37ff8d02742ecd0f1001bf3bff568caa
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-11-12 16:01:35 -0500

    add collatz conjeture steps count function

---
+++ haskell/first-steps/collatz.hs
@@ -0,0 +1,12 @@
+rawCollatz :: Integer -> Integer -> Maybe Integer
+rawCollatz n acc
+  | n <= 0 = Nothing
+  | n == 1 = Just acc
+  | even n = rawCollatz (n `div` 2) (acc + 1)
+  | otherwise = rawCollatz ((3 * n) + 1) (acc + 1)
+
+collatz :: Integer -> Maybe Integer
+collatz n = rawCollatz n 0
+
+main =
+  print (collatz 6)
